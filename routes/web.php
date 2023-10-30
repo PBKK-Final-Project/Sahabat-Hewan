0f3e5c0838d2104e87285bbadaa68db745c53742
Dimas Fadilah Akbar <5025211010@mhs.its.ac.id>
2023-10-31 01:13:22 +0700

    feat: landing page

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -26,14 +15,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/home', function () {
+        return view("home.home");
+    });
+    
+    Route::get('/landing', function () {
+        return view('landingPage.landingPage');
+    });
+    
 });
 
-Route::get('/home', function () {
-    return view("home.home");
-});
-
-Route::get('/landing', function () {
-    return view('landingPage.landingPage');
-});
 
 require __DIR__.'/auth.php';
