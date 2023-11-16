eed45ee2dc3520934f5ae2686dddda8859c090e9
Andrian Tambunan <andriantambunan2752@gmail.com>
2023-11-17 00:27:53 +0700

    fix: academy page

--- resources/views/academy/academy.blade.php
+++ resources/views/academy/academy.blade.php
@@ -1,41 +1,65 @@
 @extends('layouts.mainLayout')
 @section('content')
     @include('navbar.navbar')
-    
+
+    <div id="toast-interactive" class="mt-16 w-full max-w-[30rem] p-4 mx-auto text-gray-500 bg-white rounded-lg shadow dark:bg-gray-800 dark:text-gray-400" role="alert">
+    <div class="flex">
+
+        <div class="ml-3 text-sm font-normal">
+            <span class="mb-1 text-sm font-semibold text-gray-900 dark:text-white" >Hi, Aelita Stones!</span>
+            <div class="mb-2 text-sm font-normal">Here are your SeAn points, don't forget to use them before December 31, 2023</div> 
+            <div class="grid grid-cols-2 gap-2">
+                <div>
+                    <a href="#" class="inline-flex justify-center w-full px-2 py-1.5 text-xs font-medium text-center text-white rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-800 bg-purple-400 hover:bg-purple-500">Your Points: 3000</a>
+                </div>
+                <div>
+                    <a href="#" class="inline-flex justify-center w-full px-2 py-1.5 text-xs font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-600 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">Exchange</a> 
+                </div>
+            </div>    
+        </div>
+        <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-white items-center justify-center flex-shrink-0 text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-interactive" aria-label="Close">
+            <span class="sr-only">Close</span>
+            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+            </svg>
+        </button>
+    </div>
+</div>
+
 <h1 class="pt-24 mb-4 text-8xl font-baloo font-[700] text-[3rem] text-center text-black uppercase" style="letter-spacing: 2px;">
-              ACADEMY
+            ACADEMY
             </h1>
+<p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400 text-center">Find Some Information About Your Maw Paw Here !</p>
 
 <!-- <div class="justify-center items-center py-4 flex">
-<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-auto">
-    <a href="#">
-        <img class="rounded-t-lg" src="{{ asset('images/Anjing.png') }}" alt="" />
-    </a>
-    <div class="p-5">
+    <div class="max-w-[32rem] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-        </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover-bg-blue-700 dark:focus:ring-blue-800">
-            Read more
-            <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
+            <h5 class="p-5 text-1xl font-bold text-[18px] tracking-tight text-[#FFEBFF] dark:text-white bg-purple-400 text-center rounded-t-lg">Noteworthy technology acquisitions 2021</h5>
+            <img class="rounded-b-lg" src="{{ asset('images/orang-anjing2.png') }}" alt="" />
         </a>
     </div>
-</div>
 </div> -->
 <div class="justify-center items-center py-4 flex">
-    <div class="max-w-[35rem] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-            <h5 class="p-5 mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white bg-purple-400 text-center rounded-t-lg">Noteworthy technology acquisitions 2021</h5>
-        </a>
-        <a href="#">
-            <img class="rounded-b-lg" src="{{ asset('images/Anjing.png') }}" alt="" />
-        </a>
+    <div class="w-[44rem]">
+        <div class="mb-[2rem]">
+            <a href="#">
+                <h5 class="p-4 text-1xl font-bold text-[18px] tracking-tight text-[#FFEBFF] dark:text-white bg-purple-400 text-center rounded-t-lg">Noteworthy technology acquisitions 2021</h5>
+                <div class="relative">
+                    <img class="w-full h-60 object-cover rounded-b-lg" src="{{ asset('images/orang-anjing2.png') }}" alt="" />
+                </div>
+            </a>
+        </div>
+
+        <div class="mb-[2rem]">
+            <a href="#">
+                <h5 class="p-4 text-1xl font-bold text-[18px] tracking-tight text-[#FFEBFF] dark:text-white bg-purple-400 text-center rounded-t-lg">Noteworthy technology acquisitions 2021</h5>
+                <div class="relative">
+                    <img class="w-full h-60 object-cover rounded-b-lg" src="{{ asset('images/image-5.png') }}" alt="" />
+                </div>
+            </a>
+        </div>
     </div>
 </div>
-
 
 
     <!-- <div class="justify-center items-center py-4 flex">
